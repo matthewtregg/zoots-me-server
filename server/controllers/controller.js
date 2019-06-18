7735d11def8f02e96bd526533c99da756708562b
matthewtregg <tregearmatthew@gmail.com>
2019-06-18 09:26:51 +0200

    fix: working with FE

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -3,6 +3,7 @@ const events = require('../models/model.js');
 exports.getAllPhotosData = async (request, response) => {
   try {
     const result = await events.find();
+    console.log(result,'get')
     response.status(200).send(result);
   } catch (e) {
     response.status(500).send();
@@ -11,8 +12,9 @@ exports.getAllPhotosData = async (request, response) => {
 
 exports.addPhotoData = async (request, response) => {
   try {
-    console.log('hello world')
+    
     const result = await new events(request.body).save();
+    console.log(result,'post')
     response.status(200).send(result);
   } catch (e) {
     response.status(500).send();
--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/zoots-me', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/zootsme', {useNewUrlParser: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
